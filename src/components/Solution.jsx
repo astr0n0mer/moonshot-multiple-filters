5a6c1e863b72a4066fcbfc31b6fb245ff31e59e4
Imran Khan (MAQ LLC) <v-khanimr@microsoft.com>
2023-05-21 14:48:25 +0530

    Remove unused imports

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import "./style.css";
 
-import { items } from "./items";
 import { filters } from "./filters";
+import { items } from "./items";
 
 export default function Solution() {
   const [activeFilters, setActiveFilters] = useState([]);
